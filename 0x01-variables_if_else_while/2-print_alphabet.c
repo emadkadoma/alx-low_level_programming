c1f5db4e5ec575a2faa3b74818a53da25a862c81
emadkadoma <emadkadoma1@gmail.com>
2023-06-17 06:02:41 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: function that writes alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while( ch <= 'z' )
+	{putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
