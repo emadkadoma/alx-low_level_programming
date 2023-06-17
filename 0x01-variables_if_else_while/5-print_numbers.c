94249b635b936056c9c068b58c1fe9672cb89a04
emadkadoma <emadkadoma1@gmail.com>
2023-06-17 07:02:27 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%d \n", n);
+		n++;
+	}
+
+	return (0);
+}
