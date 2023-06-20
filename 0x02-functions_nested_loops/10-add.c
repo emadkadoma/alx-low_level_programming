62d0bd7216db486ab8ef3ad414b8dadb82344bae
emadkadoma <emadkadoma1@gmail.com>
2023-06-20 16:26:11 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Description: sum
+ *
+ * @a: first
+ *
+ * @b: second
+ *
+ * Return: 0 if true
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
