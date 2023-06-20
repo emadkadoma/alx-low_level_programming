aa967aac6b1ee2d08f51246dd8b3a3d0bccff630
emadkadoma <emadkadoma1@gmail.com>
2023-06-20 13:40:46 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: detect lower case
+ *
+ * @n: number
+ *
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
