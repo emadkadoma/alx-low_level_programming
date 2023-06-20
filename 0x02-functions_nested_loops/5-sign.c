e9383c0d87c9ca9eee1e7bc352a3c446299fbf11
emadkadoma <emadkadoma1@gmail.com>
2023-06-20 12:23:16 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Description: print sign
+ *
+ * @n: charactar to be checked
+ *
+ * Return: + if positive
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
