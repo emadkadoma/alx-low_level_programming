17056ed547f907db2e9ce84c6ee16f57b5b37d6f
emadkadoma <emadkadoma1@gmail.com>
2023-06-20 10:13:34 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: function that writes alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a' ; l <= 'z' ; l++)
+	{
+		_putchar(l);
+		_putchar('\n');
+	}
+}
