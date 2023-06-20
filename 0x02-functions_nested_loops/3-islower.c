a063e90c59a85e28bb9285755f5994239149e129
emadkadoma <emadkadoma1@gmail.com>
2023-06-20 11:27:20 -0700

    4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Description: detect lower case
+ *
+ * Return: 1 if true
+ */
+int _islower(int c)
+{
+	if (c <= 'z')
+	{
+		_putchar(1);
+	}
+	return (1);
+}
