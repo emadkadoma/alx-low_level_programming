890ce567c80af905d14d7a3a9e576e77e3cc0281
emadkadoma <emadkadoma1@gmail.com>
2023-06-22 12:01:00 -0700

    0

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,11 +7,17 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0 ; i < n ; i++)
 	{
+		for (j = 0; j < n; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
