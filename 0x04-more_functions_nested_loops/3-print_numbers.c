8d3df2ef1419cd5a7edc8534157b1c5aa76f58fe
emadkadoma <emadkadoma1@gmail.com>
2023-06-22 07:25:53 -0700

    0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0123456789\n
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+}
