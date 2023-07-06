59681f7fc0f79a54a5e4587d614b841caf4dd625
emadkadoma <emadkadoma1@gmail.com>
2023-07-05 17:01:14 -0700

    0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,43 @@
 #include "main.h"
 
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
 /**
- * sqrt2 - Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
  *
- * Return: On success 1.
- * On error, -1 is returned
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
  */
-int sqrt2(int a, int b)
+int is_divisible(int num, int div)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
+	if (num % div == 0)
+		return (0);
+
+	if (div == num / 2)
+		return (1);
+
+	return (is_divisible(num, div + 1));
 }
+
 /**
- * _sqrt_recursion - returns the natural square root of n
- * @n: Number Integer
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
  *
- * Return: On success 1.
- * On error, -1 is returned
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-	return (sqrt2(n, 1));
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
 }
