66432de68c56d62a5334eb242aaa83270981093b
emadkadoma <emadkadoma1@gmail.com>
2023-07-08 06:28:00 -0700

    0

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Write a program that prints all arguments it receives
- *        All arguments should be printed, including the first on
+ *        All arguments should be printed
  *
  * @argc: This is the argument count 
  * @argv: This is the argument vector
